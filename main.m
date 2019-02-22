45a677b5eb50afddd0dfd1ffd5798284624e98ac
Daniel Rolnik <danrol92@gmail.com>
2019-02-22 15:46:43 +0200

    STE added, vec2frames from last project

---
+++ main.m
@@ -0,0 +1,10 @@
+
+
+
+
+Tw = 20;                % analysis frame duration (ms)
+Ts = 10;                % analysis frame shift (ms)
+
+Nw = round( 1E-3*Tw*fs );    % frame duration (samples)
+Ns = round( 1E-3*Ts*fs );    % frame shift (samples)
+
