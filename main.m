34af2d4110421b53a8d7cac1761f5318d8c5cb97
Daniel Rolnik <danrol92@gmail.com>
2019-02-22 15:56:59 +0200

    added framing

--- main.m
+++ main.m
@@ -1,10 +1,17 @@
 
+close all; clear all;
 
-
+% read sound 
+[data, fs] = audioread('shee_mono.wav');
+% normalize data
+data = data / abs(max(data));
 
 Tw = 20;                % analysis frame duration (ms)
 Ts = 10;                % analysis frame shift (ms)
 
 Nw = round( 1E-3*Tw*fs );    % frame duration (samples)
 Ns = round( 1E-3*Ts*fs );    % frame shift (samples)
+
+frames = vec2frames( data, Nw, Ns, 'cols', @hamming, false); 
+
 
