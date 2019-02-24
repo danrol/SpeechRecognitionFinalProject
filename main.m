b2a1c44264765e38532030530c0426498e5cd02f
Daniel Rolnik <danrol92@gmail.com>
2019-02-24 11:35:31 +0200

    changed to avg zcr, added subplots

--- main.m
+++ main.m
@@ -14,7 +14,8 @@ ZCR_values_per_frame = ZCR(frames, f_d, fs, data);
 f_energy_vector =  STECalc(frames);
 
 ste_threshold = 0.01;
-zcr_threshold = 0.2;
+%zcr_threshold = 0.2;
+zcr_threshold = mean(ZCR_values_per_frame); %take average ZCR as threshold
 
 voiced_id = find_voiced_id(ZCR_values_per_frame, f_energy_vector, zcr_threshold, ste_threshold);
 
@@ -28,13 +29,23 @@ data_unvoiced = reshape(fr_unvoiced',1,[]);
 fr_voiced = frames(voiced_id,:); 
 data_voiced = reshape(fr_voiced',1,[]);
 
-figure
-plot(data); hold on;
+%figure
+%plot(data); hold on;
 %plot(data_unvoiced, 'b'); 
-plot(data_voiced, 'g');
+%plot(data_voiced, 'g');
 %sound(data_voiced, fs);
-title ("Blue - original data, green - voiced areas after unvoiced deleted");
+%title ("Blue - original data, green - voiced areas after unvoiced deleted");
 [ voiced_timing, unvoiced_timing ] = return_voiced_unvoiced_timings(voiced_id, unvoiced_id, f_d, frames);
+
+
+subplot(2,1,1)
+plot(data)
+title('Original Audio Data')
+
+subplot(2,1,2)
+plot(data_voiced, 'g')
+title('Only voiced part')
+
 
 
 
