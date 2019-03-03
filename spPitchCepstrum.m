8e9c23d94df1795f05dfd0feab899257b3aabc06
eg7eg7 <eg7eg7@gmail.com>
2019-03-03 13:32:47 +0200

    remove files

--- spPitchCepstrum.m
+++
@@ -1,7 +0,0 @@
-function [f0] = spPitchCepstrum(c, fs)
- % search for maximum  between 2ms (=500Hz) and 20ms (=50Hz)
- ms2=floor(fs*0.002); % 2ms
- ms20=floor(fs*0.02); % 20ms
- [maxi,idx]=max(abs(c(ms2:ms20)));
- f0 = fs/(ms2+idx-1);
-end
